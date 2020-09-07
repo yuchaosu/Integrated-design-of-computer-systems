b01017148d90042405f9d52a6e11efe582329ea9
SYC <su.yuchao@outlook.com>
2020-09-07 19:17:36 +0800

    8位跑马灯

---
+++ 8-bit LED/8-bit.srcs/sources_1/new/ctc9.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2020/09/07 19:06:32
+// Design Name: 
+// Module Name: ctc9
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ctc9(
+
+    );
+endmodule
